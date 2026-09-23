c374b332991e95a1d8aaa49eb40b259d5ec768f3
youssef sayed <youssefsayedcs1@gmail.com>
2026-09-23 20:16:38 +0300

    Implement Database class and refactor index.php for database connection and post retrieval

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 
-// Connect to the MySQL database.
-$dsn = 
-"mysql:host=localhost;
-port=3306;
-dbname=myapp;user=root;
-password=123;
-charset=utf8mb4";
+require 'functions.php';
+//require 'router.php';
+require 'Database.php';
 
-// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
-$pdo = new PDO($dsn);
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $pdo->prepare("select * from posts ");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
