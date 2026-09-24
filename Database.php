05d7e29bbbbcca510f3c7a98dbad02d43384153b
youssef sayed <youssefsayedcs1@gmail.com>
2026-09-24 20:52:07 +0300

    Refactor Database constructor to use an empty password and update view files for consistent formatting; add 404 error page.

--- Database.php
+++ Database.php
@@ -4,7 +4,7 @@ class Database
 {
     public $connection;
 
-    public function __construct($config, $username = 'root', $password = '123')
+    public function __construct($config, $username = 'root', $password = '')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
