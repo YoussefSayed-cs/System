3cd8552e75d5b82ee8539afd45e4bfe2b1457ab0
youssef sayed <youssefsayedcs1@gmail.com>
2026-09-25 17:48:55 +0300

    Add notes functionality with views and routing

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+
 require 'functions.php';
 require 'router.php';
 require 'Database.php';
+require 'router.php';
 
 $config = require('config.php');
 $db = new Database($config['database']);
